0ee14d9709f766b470c5ac36f090b51460d93b4d
daria-koivisto <daria.koivisto@tktk.ee>
2023-11-13 21:52:18 +0200

    week5

---
+++ Week5AndOperatorWithRanges/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter the current temperature:");
+int currentTemp = Int32.Parse(Console.ReadLine());
+if(currentTemp <= 0)
+{
+    Console.WriteLine("Freezing");
+}
+else if(currentTemp > 0 && currentTemp < 5)
+{
+    Console.WriteLine("Cold");
+}
+else if (currentTemp >= 5 && currentTemp < 10)
+{
+    Console.WriteLine("Chilly");
+}
+else if (currentTemp >= 10 && currentTemp < 15)
+{
+    Console.WriteLine("Cool");
+}
+else if (currentTemp >= 15 && currentTemp < 28)
+{
+    Console.WriteLine("Warm");
+}
+else
+{
+    Console.WriteLine("Boiling hot");
+}
---
+++ Week5AndOrLogicalOperators/Program.cs
@@ -0,0 +1,21 @@
+﻿string username, password;
+Console.WriteLine("Enter username:");
+username = Console.ReadLine();
+Console.WriteLine("Enter password:");
+password = Console.ReadLine();
+if (username!= "admin" || password!= "admin1234")
+{
+    Console.WriteLine("Invalid user credentials");
+}
+else
+{
+    Console.WriteLine("Welcome!");
+}
+//if (username == "admin" && password == "admin1234")
+//{
+//    Console.WriteLine("Welcome");
+//}
+//else
+//{
+//    Console.WriteLine("Invalid user credentials");
+//}
